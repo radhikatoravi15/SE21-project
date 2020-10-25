e65ce0624db8bcdce1ab9a5f71fea3e4e8a63261
shruti-k <shrutik1405@gmail.com>
2020-10-25 19:29:07 -0400

    Create app.py

---
+++ Code/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+from ensemble import *
+from user_based import *
+from item_based import *
+app = Flask(__name__,template_folder='.')
+
+@app.route("/")
+def landingpage():
+    return render_template('landingPage.html')
+
+
+@app.route('/recommend',methods = ['POST'])
+def recommend_movie():
+    userid = request.form['userid']
+    genre = request.form['genre']
+    approach = request.form['approach']
+    response = None
+    if approach == 'item-based':
+        response = get_rec_item(int(userid), genre)
+    elif approach == 'user-based':
+        response = get_rec_user(int(userid), genre)
+    elif approach == 'ensemble':
+        response = ensemble(userid, genre)
+    return render_template('response.html',data = response)
+
+
+if __name__ == "__main__":
+    app.run()
+
